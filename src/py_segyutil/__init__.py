20035f42206153f014429e27df1ea29aef6f5723
Jaggs <kevin.jaggs@ineos.com>
2026-01-02 10:11:01 +0000

    Revised start up files

--- src/py_segyutil/__init__.py
+++ src/py_segyutil/__init__.py
@@ -1,1 +1,5 @@
 
+from .core import SegyUtil
+
+__all__ = ["SegyUtil"]
+__version__ = "0.1.0"
--- src/py_segyutil/core.py
+++ src/py_segyutil/core.py
@@ -1,2 +1,8 @@
-def run() -> None:
-    print("Running from core")
+class SegyUtil:
+
+    def __init__(self, name: str= "Python SEGY Utility") -> None:
+
+        self.name = name
+    def run_test(self) -> None:
+
+        print(f"✅ Hello {self.name}, class ran successfully")
