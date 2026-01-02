8ad8c6e2370195fea26ab75c249e58f058ac5feb
Jaggs <kevin.jaggs@ineos.com>
2026-01-02 12:20:38 +0000

    revised file check

--- src/py_segyutil/utils/file_check.py
+++ src/py_segyutil/utils/file_check.py
@@ -9,6 +9,8 @@ def segy_dir_check(in_segy_dir = None):
     if os.path.isdir(in_segy_dir) == False:
         print(f"Directory {in_segy_dir} does not exist!")
         sys.exit()
+    else:
+        print(f"Valid file directory")
     
 def segy_file_check(in_segy_file=None):
     '''
@@ -18,6 +20,8 @@ def segy_file_check(in_segy_file=None):
     if os.path.isfile(in_segy_file) == False:
         print(f"File {in_segy_file} does not exist!")
         sys.exit()
+    else:
+        print(f"Valid file name")
 
 def segy_file_valid(qc_path = None):
 
