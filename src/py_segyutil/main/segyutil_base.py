b2856d7bf792cd488adab18554a80bbbe8136a08
Jaggs <kevin.jaggs@ineos.com>
2026-01-07 09:52:52 +0000

    Update segyutil_base tarce Validation

--- src/py_segyutil/main/segyutil_base.py
+++ src/py_segyutil/main/segyutil_base.py
@@ -27,7 +27,7 @@ class SegyUtil:
             self.big_endian = 'little'
 
         #assign all class variables to a dictionary - appears to dynamically update....
-        self.segy_dict =  vars(self)
+        #self.segy_dict =  vars(self)
 
 
     def read_segy(self,segy_path = None):
@@ -44,40 +44,45 @@ class SegyUtil:
 
             self.segy_infile_reader_binary.close()
 
-
         self.number_bytes_per_sample,self.trace_format_code_string = sample_format_code(trace_format_code=self.segy_binary['Data Sample Format Code'][2])
 
+        if self.segy_binary['Sample Interval In Microseconds'][2] == 1:
+            self.data_type = "Stack"
+        else:
+            self.data_type = "Gather"            
+
         self.number_bytes_per_trace_data = self.segy_binary['Number Samples Per Data Trace'][2] * self.number_bytes_per_sample
-        self.number_bytes_per_trace_package = self.number_bytes_per_trace_data * self.segy_binary['No. Data Traces Per Ensemble'][2]
+        self.number_bytes_per_trace_package = self.number_bytes_per_trace_data + self.bytes_trace_header
+        self.sample_rate = self.segy_binary['Sample Interval In Microseconds'][2]
+
+        self.segy_trace_validation()
+
+    def segy_trace_validation(self):
 
         #calculate header and databyte portions of input file
-        self.total_header_traces = self.bytes_ebcdic + self.bytes_bin_trace_header + (self.segy_binary['Number Of Extended Textual Records'][2]*self.bytes_extended_textual_header)
-        self.total_data_traces = self.segy_size - self.total_header_traces
+        self.total_header_bytes = self.bytes_ebcdic + self.bytes_bin_trace_header + (self.segy_binary['Number Of Extended Textual Records'][2]*self.bytes_extended_textual_header)
+        self.total_data_bytes = self.segy_size - self.total_header_bytes
 
         #determine that number data trace bytes / bytes per package is an integer multiple, print warngin if not case
-        if self.total_data_traces % self.number_bytes_per_trace_package != 0:
+        if self.total_data_bytes % self.number_bytes_per_trace_package != 0:
             print("WARNING - expected number of traces is not an INTEGER - CHECK MANUALLY")
 
         #calculate expected number of data traces within the file
-        self.expected_number_of_traces = int(self.total_data_traces / self.number_bytes_per_trace_package)  
+        self.expected_number_of_traces = int(self.total_data_bytes / self.number_bytes_per_trace_package)  
 
     def read_segy_summary(self):
 
         print("\n")
         print('Total number bytes input file = {0}'.format(self.segy_size))
+        print('Sample Rate = {0}'.format(self.segy_binary['Sample Interval In Microseconds'][2]))
         print('Number of samples per trace = {0}'.format(self.segy_binary['Number Samples Per Data Trace'][2]))
         print('Number of bytes per sample = {0}'.format(self.bytes_per_sample))
+        print('Number of bytes per sample = {0}'.format(self.segy_binary['Ensemble Fold'][2]))
         print('Data format = {0}'.format(self.trace_format_code_string))
         print('Number of bytes per trace data = {0}'.format(self.number_bytes_per_trace_data))
-        print('Number of bytes per ensemble = {0}'.format(self.number_bytes_per_trace_package))
-        print('Number of bytes per trace ensemble incl. header = {0}'.format(self.number_bytes_per_trace_package +self.bytes_trace_header)) 
+        print('Number of bytes per trace ensemble incl. header = {0}'.format(self.number_bytes_per_trace_package)) 
         print('Number of expected textual header files = {0}'.format(self.segy_binary['Number Of Extended Textual Records'][2])) 
         print('\nExpected number of seismic traces = {0}'.format(self.expected_number_of_traces)) 
-
-
-
-
-
 
 
     def run(self) -> None:
